b3beade58ad323c3bc114c52bb08ee83ac3898f2
saitotm <tomokazu.ml.tech@gmail.com>
2022-02-28 17:44:57 +0900

    implement simple cui

--- src/io/cui.rs
+++ src/io/cui.rs
@@ -2,40 +2,53 @@ use crate::core::board::{Board, Position};
 use crate::core::ruversi::{TurnPlayer, GameResult};
 use super::super::core::ruversi::IO;
 
-struct CUI {
+struct CUI;
 
+impl CUI {
+    fn new() -> Self {
+        Self
+    }
 }
 
 impl IO for CUI {
     fn game_start(&self, board: &Board) {
-        unimplemented!()
+        println!("Game Start");
+        println!("{}", board);
     }
 
     fn skip_turn(&self, turn: &TurnPlayer) {
-        unimplemented!()
+        println!("There is no place to a {} disk.", turn);
     }
 
     fn start_turn(&self, turn: &TurnPlayer) {
-        unimplemented!()
+        println!("{} turn:", turn);
     }
 
     fn before_mov(&self, board: &Board, turn: &TurnPlayer) {
-        unimplemented!()
+        println!("{} move", turn);
     }
 
     fn after_illegal_mov(&self, pos: &Position, turn: &TurnPlayer) {
-        unimplemented!()
+        println!("A disk cannot be placed on {}", pos);
     }
 
     fn after_mov(&self, pos: &Position, turn: &TurnPlayer) {
-        unimplemented!()
+        println!("A disk be place on {}", turn);
     }
 
     fn after_update(&self, board: &Board) {
-        unimplemented!()
+        println!("{}", board);
     }
 
     fn game_end(&self, board: &Board, result: &GameResult) {
-        unimplemented!()
+        println!("Result");
+        println!("{}", board);
+        println!("Dark vs Light");
+        println!("{} : {}", result.dark_disks, result.light_disks);
+
+        match result.winner {
+            None => println!("Draw"),
+            Some(player) => println!("{} WIN", player),
+        }
     }
 }
